662a63a6d4159a5e92f89ef5c81e26550c13e519
gong.bo <bo_gong@suishouji.com>
2019-10-09 20:15:06 +0800

    gong.bo

--- app/src/main/java/com/gongbo/fss/demo/bind/BindDetailActivity.java
+++ app/src/main/java/com/gongbo/fss/demo/bind/BindDetailActivity.java
@@ -7,16 +7,18 @@ import com.gongbo.fss.bind.annotation.BindActivity;
 import com.gongbo.fss.bind.annotation.BindBindingParam;
 import com.gongbo.fss.bind.annotation.BindOnClick;
 import com.gongbo.fss.bind.annotation.BindParam;
+import com.gongbo.fss.demo.BR;
 import com.gongbo.fss.demo.R;
 import com.gongbo.fss.demo.databinding.ActivityBindDetailBinding;
 import com.gongbo.fss.router.annotation.Route;
 
+
 @Route
 @BindActivity(layout = R.layout.activity_bind_detail, finish = R.id.btn_finish)
-@BindBindingParam(key = "EXTRA_VALUE1", id = com.gongbo.fss.demo.BR.value1)
+@BindBindingParam(key = "EXTRA_VALUE1", id = BR.value1)
 public class BindDetailActivity extends BaseBindingFssActivity<ActivityBindDetailBinding> {
 
-    @BindBindingParam(key = "EXTRA_VALUE2", id = com.gongbo.fss.demo.BR.value2)
+    @BindBindingParam(key = "EXTRA_VALUE2", id = BR.value2)
     private String value2;
 
     @BindParam(key = "EXTRA_VALUE3")
