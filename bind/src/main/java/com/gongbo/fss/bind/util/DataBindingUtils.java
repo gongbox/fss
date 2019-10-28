4000aed6756e3fe8d62dd6b0f4ae34ea024d4789
gong.bo <bo_gong@suishouji.com>
2019-10-28 19:29:24 +0800

    gong.bo

--- bind/src/main/java/com/gongbo/fss/bind/util/DataBindingUtils.java
+++ bind/src/main/java/com/gongbo/fss/bind/util/DataBindingUtils.java
@@ -6,6 +6,7 @@ import androidx.databinding.ViewDataBinding;
 import com.gongbo.fss.common.util.ReflectUtils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 
 public class DataBindingUtils {
@@ -30,5 +31,48 @@ public class DataBindingUtils {
                 binding.setVariable(bindingId, value);
             }
         }
+    }
+
+    /**
+     * 绑定databinding变量
+     *
+     * @param obj
+     * @param bindingName
+     * @param value
+     */
+    public static void bindingVariable(Object obj, String bindingFieldName, String bindingName, Object value) {
+        Field bindingField = ReflectUtils.getField(obj.getClass(), bindingFieldName);
+        ViewDataBinding binding = null;
+        if (bindingField != null) {
+            binding = ReflectUtils.getFieldValue(obj, bindingField);
+        }
+        if (binding != null) {
+            if (value != null) {
+                String methodName = "set" + capitalizeString(bindingName);
+                Method method = ReflectUtils.getMethod(binding.getClass(), methodName, value.getClass());
+                if (method != null) {
+                    ReflectUtils.methodInvoke(binding, method, value);
+                } else {
+                    throw new RuntimeException("在" + binding.getClass().getCanonicalName() + "类中找不到方法：" + methodName);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 首字母转化为大写
+     *
+     * @param string
+     * @return
+     */
+    public static String capitalizeString(String string) {
+        if (string == null) {
+            return null;
+        }
+        if (string.isEmpty()) {
+            return string;
+        }
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
 }
