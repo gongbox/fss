c6d43ef2871f18996af77350c52efd4b8812679c
gong.bo <bo_gong@suishouji.com>
2019-11-04 11:40:39 +0800

    gong.bo

--- router-compiler/src/main/java/com/gongbo/fss/router/utils/FieldUtils.java
+++ router-compiler/src/main/java/com/gongbo/fss/router/utils/FieldUtils.java
@@ -2,24 +2,21 @@ package com.gongbo.fss.router.utils;
 
 import com.squareup.javapoet.FieldSpec;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FieldUtils {
-    public static boolean containsField(List<FieldSpec> fieldSpecs, String name) {
-        if (fieldSpecs == null) return false;
-        if (name == null) return false;
-        for (FieldSpec fieldSpec : fieldSpecs) {
-            if (fieldSpec.name.equals(name)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     public static String getFieldName(List<FieldSpec> fieldSpecs, String name) {
+        if (fieldSpecs == null) return name;
+        List<String> names = new ArrayList<String>(fieldSpecs.size());
+        for (FieldSpec fieldSpec : fieldSpecs) {
+            names.add(fieldSpec.name);
+        }
+
         int i = 1;
         String newName = name;
-        while (containsField(fieldSpecs, newName)) {
+        while (names.contains(newName)) {
             newName = name + i++;
         }
         return name;
