37422bb0bef2b16b6ffb0648a86224f3d811c02e
gongbox <Gb090793>
2019-11-30 23:12:42 +0800

    gong.bo androidx

---
+++ testmoudle/src/main/java/com/fss/testmoudle/TestMainActivity.java
@@ -0,0 +1,17 @@
+package com.fss.testmoudle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fss.router.annotation.Route;
+
+//@Route
+public class TestMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_main);
+    }
+}
